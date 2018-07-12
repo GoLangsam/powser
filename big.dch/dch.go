53914ffbea5b2ccb4dbb330df0413eb5f94116c9
GoLangsam <GoLangsam.APA@a-bc.net>
2018-07-12 05:42:42 +0200

    rename dch directories

--- big.dch/dch.go
+++ big.dch/dch.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package dch // import "github.com/GoLangsam/powser/dch.big"
+package dch // import "github.com/GoLangsam/powser/big.dch"
 
 import (
 	"math/big"
-	// "github.com/GoLangsam/powser/big"
+	// "github.com/GoLangsam/powser/rat"
 )
 
 // ===========================================================================
